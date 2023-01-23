81e3d91da0fbd5927e279071234f75c770346585
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-01-22 21:10:51 -0300

    Criando repositório para Festa

--- src/main/java/pi/de/diamondevents/controllers/FestasController.java
+++ src/main/java/pi/de/diamondevents/controllers/FestasController.java
@@ -1,14 +1,19 @@
 package pi.de.diamondevents.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pi.de.diamondevents.models.Festa;
+import pi.de.diamondevents.repositories.FestaRepository;
 
 @Controller
 public class FestasController {
 
+	@Autowired
+	private FestaRepository fr;
+	
 	@RequestMapping("/festas/form")
 	public String form() {
 		return "formFesta";
@@ -18,6 +23,7 @@ public class FestasController {
 	public String adicionarFesta(Festa festa) {
 		
 		System.out.println(festa);
+		fr.save(festa);
 		
 		return "festa-adicionada";
 	}
---
+++ src/main/java/pi/de/diamondevents/repositories/FestaRepository.java
@@ -0,0 +1,9 @@
+package pi.de.diamondevents.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pi.de.diamondevents.models.Festa;
+
+public interface FestaRepository extends JpaRepository<Festa, Long>{
+
+}
