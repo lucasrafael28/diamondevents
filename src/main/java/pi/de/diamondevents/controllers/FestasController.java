7fdb7274468c54871f2d6dbcde1aa870376c32ec
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-01-22 21:56:07 -0300

    Criando página de detalhes da festa

--- src/main/java/pi/de/diamondevents/controllers/FestasController.java
+++ src/main/java/pi/de/diamondevents/controllers/FestasController.java
@@ -1,10 +1,12 @@
 package pi.de.diamondevents.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -39,5 +41,22 @@ public class FestasController {
 		ModelAndView mv = new ModelAndView("festas/lista");
 		mv.addObject("festas", festas);
 		return mv;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalharFesta(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Festa> opt = fr.findById(id);
+
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/festas");
+			return md;
+		}
+
+		md.setViewName("festas/detalhes");
+		Festa festa = opt.get();
+		md.addObject("festa", festa);
+
+		return md;
 	}
 }
