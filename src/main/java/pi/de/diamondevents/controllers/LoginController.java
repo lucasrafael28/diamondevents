d726fbeadb6e0dfaaa4dc357359b2e3e501e828e
marcelakaroline1126 <marcela.karoline@escolar.ifrn.edu.br>
2023-01-30 15:52:29 -0300

    Páginas e controller de login

---
+++ src/main/java/pi/de/diamondevents/controllers/LoginController.java
@@ -0,0 +1,24 @@
+package pi.de.diamondevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login/login";
+	}
+
+	@PostMapping("/login")
+	public String fazerLogin(String matricula, String senha) {
+		return "login/login";
+	}
+
+	@GetMapping("/logout")
+	public String logout() {
+		return "login/logout";
+	}
+}
