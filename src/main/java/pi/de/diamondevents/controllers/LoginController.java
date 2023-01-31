065fc4f8a9ad343ab48e8924c9e4ee09cb9fda1e
marcelakaroline1126 <marcela.karoline@escolar.ifrn.edu.br>
2023-01-31 10:29:49 -0300

    Primeiras configurações com Spring Security

--- src/main/java/pi/de/diamondevents/controllers/LoginController.java
+++ src/main/java/pi/de/diamondevents/controllers/LoginController.java
@@ -2,18 +2,12 @@ package pi.de.diamondevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
 	public String login() {
-		return "login/login";
-	}
-
-	@PostMapping("/login")
-	public String fazerLogin(String matricula, String senha) {
 		return "login/login";
 	}
 
