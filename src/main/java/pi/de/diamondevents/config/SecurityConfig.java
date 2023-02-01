4aca63b909ee64e0f06afb44501439c64eb868c5
marcelakaroline1126 <marcela.karoline@escolar.ifrn.edu.br>
2023-02-01 09:23:08 -0300

    Concentrando configuração de segurança na classe SecurityConfiguration.

--- src/main/java/pi/de/diamondevents/config/SecurityConfig.java
+++ src/main/java/pi/de/diamondevents/config/SecurityConfig.java
@@ -1,12 +1,25 @@
 package pi.de.diamondevents.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+	    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		builder
+			.inMemoryAuthentication()
+			.withUser("joao").password(encoder.encode("123")).roles("ADMIN", "USER")
+			.and()
+			.withUser("jose").password(encoder.encode("123")).roles("USER", "AVALIADOR");
+	}
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
