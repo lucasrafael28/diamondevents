3e9132babce797638f5e7f3d05b3be52dcd1c43d
marcelakaroline1126 <marcela.karoline@escolar.ifrn.edu.br>
2023-02-01 09:24:36 -0300

    Alterando papeis para portugues

--- src/main/java/pi/de/diamondevents/config/SecurityConfig.java
+++ src/main/java/pi/de/diamondevents/config/SecurityConfig.java
@@ -15,9 +15,9 @@ public class SecurityConfig {
 
 		builder
 			.inMemoryAuthentication()
-			.withUser("joao").password(encoder.encode("123")).roles("ADMIN", "USER")
+			.withUser("joao").password(encoder.encode("123")).roles("ADMIN", "USUARIO")
 			.and()
-			.withUser("jose").password(encoder.encode("123")).roles("USER", "AVALIADOR");
+			.withUser("jose").password(encoder.encode("123")).roles("USUARIO", "AVALIADOR");
 	}
 	
 	@Bean
