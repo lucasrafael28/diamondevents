7bffd403795b5c145ac97afec9e06f92ea39a9e0
marcelakaroline1126 <marcela.karoline@escolar.ifrn.edu.br>
2023-02-02 14:09:54 -0300

    Funcionalidade de listar usuários

---
+++ src/main/java/pi/de/diamondevents/controllers/AdminController.java
@@ -0,0 +1,29 @@
+package pi.de.diamondevents.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import pi.de.diamondevents.models.Usuario;
+import pi.de.diamondevents.repositories.UsuarioRepository;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired
+	UsuarioRepository ur;
+
+	@GetMapping("/usuarios")
+	public ModelAndView listaUsuarios() {
+
+		Iterable<Usuario> usuarios = ur.findAll();
+
+		ModelAndView mv = new ModelAndView("admin/usuarios/lista");
+		mv.addObject("usuarios", usuarios);
+
+		return mv;
+	}
+}
