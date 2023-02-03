494f279a94c45c2bdedb73bfab14e497351c4528
sabrinarafaelle02 <sabrina.valerio@escolar.ifrn.edu.br>
2023-02-03 17:42:04 -0300

    Criando funcionalidade para edição de papeis dos usuários.

---
+++ src/main/java/pi/de/diamondevents/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package pi.de.diamondevents.repositories;
+
+import javax.management.relation.Role;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long>{
+
+}
